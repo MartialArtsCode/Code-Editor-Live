22d80361fe4fcac3d74769968f4ae9921d1dbc0e
MartialArtsDevelopmentLab <ptacademy@duck.com>
2026-03-15 02:23:20 -0400

    Refactor dependency analysis and drawing logic

--- vusualization/dependencyGraph.js
+++ vusualization/dependencyGraph.js
@@ -1,16 +1,46 @@
 import {project} from "../core/projectManager.js"
+  
+const canvas=document.getElementById("graph")
+  const ctx=canvas.getContext("2d")
 
-export function analyze(){
+function draw(){
 
-const deps={
+  ctx.clearRect(0,0,canvas.width,canvas.height)
 
-htmlUsesJS:project.files.html.includes("<script"),
-htmlUsesCSS:project.files.css.length>0
+  ctx.fillStyle="white"
+
+  ctx.fillText("HTML",40,100)
+    ctx.fillText("CSS",160,40)
+      ctx.fillText("JS",160,160)
+        ctx.fillText("API",260,100)
+
+  if(project.files.css.length>0){
+
+    ctx.beginPath()
+      ctx.moveTo(60,100)
+        ctx.lineTo(160,40)
+          ctx.stroke()
+
+  }
+
+  if(project.files.js.length>0){
+
+    ctx.beginPath()
+      ctx.moveTo(60,100)
+        ctx.lineTo(160,160)
+          ctx.stroke()
+
+  }
+
+  if(project.files.js.includes("fetch")){
+
+    ctx.beginPath()
+      ctx.moveTo(160,160)
+        ctx.lineTo(260,100)
+          ctx.stroke()
+
+  }
 
 }
 
-console.log("Dependencies",deps)
-
-}
-
-setInterval(analyze,2000)
+setInterval(draw,1500)
